76b52e3599ad4b19ea08177084bc4155c8bffde1
Sreekar <sreekark789@gmail.com>
2024-06-21 17:52:29 +0530

    Home page

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -11,7 +11,7 @@ function Intro() {
                 <Card.Img src={img2} alt="Card image" className='shop-img opacity-50' />
                 <Card.ImgOverlay className='card-text text-center overflow-x-hidden d-flex flex-column justify-content-center align-items-center'>
                     <Card.Title className='card-title1 fw-bold display-3'>Welcome to $hopNow</Card.Title>
-                    <Card.Text className='card-para1 fw-bold fs-5'>
+                    <Card.Text className='card-para1 fw-bold fs-4'>
                         Discover a world of quality products at unbeatable prices. 
                     </Card.Text>
                 </Card.ImgOverlay>
@@ -20,8 +20,8 @@ function Intro() {
             <Card className="bg-success mt-3">
                 <Card.Img src={img3} alt="Card image" className='shop-img2 opacity-50' />
                 <Card.ImgOverlay className='card-text2 text-center overflow-x-hidden d-flex flex-column justify-content-center align-items-center'>
-                    <Card.Title className='card-title2 fw-bold display-5'>Your One-Stop Shop</Card.Title>
-                    <Card.Text className='card-para2 fw-bold fs-5'>
+                    <Card.Title className='card-title2 fw-bold display-4'>Your One-Stop Shop</Card.Title>
+                    <Card.Text className='card-para2 fw-bold fs-4'>
                         Everything you need, just a click away.
                     </Card.Text>
                 </Card.ImgOverlay>
