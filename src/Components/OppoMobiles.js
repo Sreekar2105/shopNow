b10b15ffdce5d364957c8009353549025a7b3c43
Sreekar <sreekark789@gmail.com>
2024-07-10 20:19:03 +0530

    breakpoints

--- src/Components/OppoMobiles.js
+++ src/Components/OppoMobiles.js
@@ -75,7 +75,7 @@ function OppoMobiles()
         
         <p className='title oppo-mobiles-title display-5 fw-bold text-center mb-3 py-2'>OPPO Mobiles</p>
         
-        <Row xs={2} md={3} lg={4} className='gx-0' >
+        <Row xs={1} md={3} lg={4} className='gx-0' >
 
 
     {
